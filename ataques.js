213a07de8ddff1f5003c0791e6b8c2e765e75ee7
heyyah <yurihass15@gmail.com>
2018-03-19 22:01:05 -0300

    Update ataques.js

--- ataques.js
+++ ataques.js
@@ -3,9 +3,13 @@
 // @version  1
 // @grant    none
 // ==/UserScript==
-
-if(document.title.substring(0,  6) == "Rua de") {
+if(document.getElementsByClassName("minfo").length != 0) {
+    if(document.getElementsByClassName("minfo")[0].textContent.indexOf("Você comprou café") != -1) {
+      window.location.href = "http://www.google.com.br";
+    }
+  } else if(document.title.substring(0,  6) == "Rua de") {
 	var x = document.getElementsByClassName("window-modal-content");
+  
 	if(x.length == 1) {
   	x = document.getElementsByClassName("merr");
 	  if(x.length == 0) {
@@ -18,7 +22,7 @@ if(document.title.substring(0,  6) == "Rua de") {
   
 	  	if(x.substring(0,28) == "Já atacou este gangster hoje") {
 				console.log("Já atacou este gangster");
-    	  //window.location.href = "http://www.google.com.br";
+    	  window.location.href = "http://www.google.com.br";
         
         var temp = (x.substring(70, 72) * 1000) + (x.substring(67, 69) * 60000) + (x.substring(64, 66) * 3600000);
         temp -= 1500;
@@ -52,7 +56,10 @@ if(document.title.substring(0,  6) == "Rua de") {
 	  }
 	} else {
   	console.log("Já atacou este gangster");
-  	window.location.href = "http://www.google.com.br";
+  	//document.getElementsByClassName("u-gauge")[1].click();
+    //window.location.href = "http://www.google.com.br";
+    document.getElementsByClassName("cofemachine")[0].click();
+    document.getElementsByClassName("st")[2].click();
 	}
 }
 
