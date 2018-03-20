c91820ef48c243180cf82ffbd5473005fac561c9
heyyah <yurihass15@gmail.com>
2018-03-19 22:01:41 -0300

    Update comprar.js

--- comprar.js
+++ comprar.js
@@ -3,10 +3,28 @@
 // @version  1
 // @grant    none
 // ==/UserScript==
-
-var item = document.getElementsByClassName("item_small");
-item[18].click();
-window.setTimeout(function() {
-var botao = document.getElementsByClassName("ok");
-botao[0].click();
-}, 1000);
+if(window.location.href.indexOf("contrabandist") != -1) {
+  var p = document.getElementsByClassName("inmb");
+  for(var x = 0; x < p.length; x++) {
+    if(p[x].parentElement.attributes[2] != null) {
+      if(p[x].parentElement.attributes[2].nodeValue.indexOf("buy_connections") != -1){
+        if(p[x].parentElement.attributes[2].nodeValue.indexOf("Pontos de escola") != -1) {
+          var item = document.getElementsByClassName("item_small");
+          item[18].click();
+          window.setTimeout(function() {
+          var botao = document.getElementsByClassName("ok");
+          botao[0].click();
+          }, 1000);
+        }
+        if(p[x].parentElement.attributes[2].nodeValue.indexOf("Pontos de Fitness") != -1) {
+          var item = document.getElementsByClassName("item_small");
+          item[19].click();
+          window.setTimeout(function() {
+          var botao = document.getElementsByClassName("ok");
+          botao[0].click();
+          }, 1000);
+        }
+      }
+    }
+  }
+}
